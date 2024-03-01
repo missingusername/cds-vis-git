a30ca860fe00544f884c3169e8b8400a313aed13
Albin <x7evaa@gmail.com>
2024-03-01 17:48:26 +0100

    vis assignment 1 done

--- assignments/assignment1/src/image_search.py
+++ assignments/assignment1/src/image_search.py
@@ -1,8 +1,12 @@
-import cv2
-import numpy as np
-import pandas as pd
 import os
+import cv2
+import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+# pip install opencv-python pandas tqdm
 
 # Get the directory where the script is located
 script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -32,7 +36,8 @@ def calculate_histogram(image_path):
     """Load an image & Calculate its color histogram."""
     image = cv2.imread(image_path)
     hist = cv2.calcHist([image], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
-    return hist
+    normalized_hist = cv2.normalize(hist, hist, 0, 1.0, cv2.NORM_MINMAX)
+    return normalized_hist
 
 def compare_histograms(hist1, hist2):
     """Compare two histograms using the chi-square distance."""
@@ -43,6 +48,26 @@ def save_results(results, output_folder):
     # Select the top 5 most similar images
     df = pd.DataFrame(results, columns=['Filename', 'Distance'])
     df.to_csv(os.path.join(output_folder, 'similar_images.csv'), index=False)
+
+def plot_images(image_data):
+    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+    
+    for i, (image_name, distance) in enumerate(image_data):
+        image_path = os.path.join('..',
+                                  'in',
+                                  'jpg',
+                                  image_name)
+        row = i // 3
+        col = i % 3
+        ax = axes[row, col]
+        
+        img = mpimg.imread(image_path)
+        ax.imshow(img)
+        ax.set_title(f"{image_name}\nDistance: {distance}")
+        ax.axis('off')
+    
+    plt.show()
 
 def main():
     # Define paths and parameters
@@ -61,6 +86,8 @@ def main():
 
     # Find similar images
     results = find_similar_images(target_image_path, dataset_folder)
+
+    plot_images(results)
 
     # Save results
     save_results(results, output_folder)
