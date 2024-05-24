59e6510678380b42e55ffed135974b841cad00ff
missingusername <x7evaa@gmail.com>
2024-05-24 05:39:32 +0200

    readme added. typo fixed in run.sh scripts

--- assignments/assignment4/win_run.sh
+++ assignments/assignment4/win_run.sh
@@ -3,7 +3,7 @@
 # Activate the virtual environment
 source env/Scripts/activate
 
-# Run the face_detection.py file, using optional comman line arguments
+# Run the face_detection.py file, using optional command line arguments
 python src/face_detection.py "$@"
 
 # Run the plot.py file afterwards
